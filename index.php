bfbcf74991bad6c8c2ae9cdd9fa66e1b7340b7dc
Naomi Paulmin <npaulmin@yahoo.com>
2017-12-11 08:49:33 +0100

    fichier index.php - affichage des annonces

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require('includes/init.php');
+
+?>
+
+    <div class="row">
+        <div class="tweets col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+            <h3>Toutes les annonces</h3>
+            <table class="table table-striped table-hover">
+                <tr>
+                    <td><b>Titre de l'annonce</b></td>
+                    <td><b>Deadline</b></td>
+                    <td><b>Description</b></td>
+                </tr>
+                <?php
+
+                $req = $db->query("SELECT * FROM ad");
+                $ads=$req->fetchAll();
+                foreach ($ads as $ad)
+                {
+                    ?>
+
+                    <tr>
+                        <td>
+                            <?php echo $ad['title']; ?>
+                        </td>
+                        <td>
+                        <?php
+                            if(isset($ad['deadline'])) {
+                                echo $ad['deadline'];
+                            }
+                        ?>
+                        </td>
+                        <td>
+                            <?php echo $ad['description']; ?>
+                        </td>
+                    </tr>
+
+
+                    <?php
+                }
+                ?>
+            </table>
+        </div>
+    </div>
