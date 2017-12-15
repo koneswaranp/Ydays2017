0746d8a4abf39b87764047ab46487990a03f262e
Naomi Paulmin <npaulmin@yahoo.com>
2017-12-15 09:32:45 +0100

    index

--- index.php
+++ index.php
@@ -5,7 +5,22 @@ require('includes/init.php');
 if(isset($_SESSION['id'])) {
 
     ?>
-
+    <!doctype html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+        <script
+                src="https://code.jquery.com/jquery-3.1.1.min.js"
+                integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+                crossorigin="anonymous"></script>
+        <script src="semantic/dist/semantic.min.js"></script>
+    </head>
+    <body>
     <div class="row">
         <div class="tweets col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
     <h2>Publier une annonce</h2>
@@ -96,3 +111,6 @@ else {
     <a href="connexion.php">Connexion</a>
 <?php
 }
+?>
+    </body>
+</html>
